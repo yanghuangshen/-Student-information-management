d73e6e6078ff5ff2535b3094a1b32e1cc0f4ee48
yanghuangshen <144525518+yanghuangshen@users.noreply.github.com>
2024-05-27 13:18:36 +0800

    Add files via upload

--- smart_campus.sql
+++ smart_campus.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 19/04/2022 12:03:05
+ Date: 27/05/2024 12:03:05
 */
 
 SET NAMES utf8mb4;
@@ -36,11 +36,8 @@ CREATE TABLE `tb_admin`  (
 -- ----------------------------
 -- Records of tb_admin
 -- ----------------------------
-INSERT INTO `tb_admin` VALUES (101, 'admin', '女', '21232f297a57a5a743894a0e4a801fc3', '111111@qq.com', '13866666666', '昌平', 'upload/default.jpg');
-INSERT INTO `tb_admin` VALUES (102, 'admin1', '男', '21232f297a57a5a743894a0e4a801fc3', '333333@qq.com', '13866666666', '北京', 'upload/default.jpg');
-INSERT INTO `tb_admin` VALUES (103, 'admin2', '男', '21232f297a57a5a743894a0e4a801fc3', '333333@qq.com', '13866666666', '北京', 'upload/default.jpg');
-INSERT INTO `tb_admin` VALUES (104, 'admin3', '男', '21232f297a57a5a743894a0e4a801fc3', '333333@qq.com', '13866666666', '宏福苑', 'upload/default.jpg');
-
+INSERT INTO `tb_admin` VALUES (101, 'admin', '男', '21232f297a57a5a743894a0e4a801fc3', '111111@qq.com', '13866666666', '湘潭', 'upload/default.jpg');
+INSERT INTO `tb_admin` VALUES (102, 'admin1', '男', '21232f297a57a5a743894a0e4a801fc3', '333333@qq.com', '13866666666', '湘潭', 'upload/default.jpg');
 -- ----------------------------
 -- Table structure for tb_clazz
 -- ----------------------------
@@ -60,13 +57,10 @@ CREATE TABLE `tb_clazz`  (
 -- ----------------------------
 -- Records of tb_clazz
 -- ----------------------------
-INSERT INTO `tb_clazz` VALUES (1, '一年一班', 30, '大圣的一年一班好', '大圣', 'dasheng@163.com', '13866666666', '一年级');
-INSERT INTO `tb_clazz` VALUES (2, '一年二班', 28, '小张的一年二班好', '小张', 'xiaozhang@163.com', '13866666666', '一年级');
-INSERT INTO `tb_clazz` VALUES (3, '二年一班', 35, '小韩的二年一班好', '小韩', 'xiaohan@163.com', '13866666666', '二年级');
-INSERT INTO `tb_clazz` VALUES (4, '二年二班', 30, '小强的二年二班好', '小强', 'xiaoqiang@163.com', '13866666666', '二年级');
-INSERT INTO `tb_clazz` VALUES (5, '三年一班', 30, '小花的三年一班好', '小花', 'xiaohua@163.com', '13866666666', '三年级');
-INSERT INTO `tb_clazz` VALUES (6, '三年二班', 30, '小赵的三年二班好', '小赵', 'xiaozhao@163.com', '13866666666', '三年级');
-INSERT INTO `tb_clazz` VALUES (7, '四年一班', 30, '小赵的三年二班好', '小飞', 'xiaofei@163.com', '13866666666', '四年级');
+INSERT INTO `tb_clazz` VALUES (1, '计算机科学与技术一班', 30, '计算机科学与技术一班', '欧', 'ou@qq.com', '13866666666', '22计算机科学与技术一班');
+INSERT INTO `tb_clazz` VALUES (2, '计算机科学与技术二班', 28, '计算机科学与技术二班', '申', 'shen@qq.com', '13866666666', '22计算机科学与技术二班');
+INSERT INTO `tb_clazz` VALUES (3, '计算机科学与技术三班', 35, '计算机科学与技术三班', '叶', 'ye@qq.com', '13866666666', '22计算机科学与技术三班');
+INSERT INTO `tb_clazz` VALUES (4, '计算机科学与技术四班', 30, '计算机科学与技术四班', '杨', 'yang@qq.com', '13866666666', '22计算机科学与技术四班');
 
 -- ----------------------------
 -- Table structure for tb_grade
@@ -85,11 +79,7 @@ CREATE TABLE `tb_grade`  (
 -- ----------------------------
 -- Records of tb_grade
 -- ----------------------------
-INSERT INTO `tb_grade` VALUES (1, '一年级', '大圣', 'dasheng@163.com', '13866666666', '大学一年级');
-INSERT INTO `tb_grade` VALUES (2, '二年级', '小魏', 'xiaowei@163.com', '13866666666', '大学二年级');
-INSERT INTO `tb_grade` VALUES (3, '三年级', '小李', 'xiaoli@163.com', '13666666666', '三年级,这个班级的孩子们很有才艺');
-INSERT INTO `tb_grade` VALUES (4, '五年级', '小丽', 'li@123.com', '13666666666', '这个年级的同学多才多活力');
-INSERT INTO `tb_grade` VALUES (5, '六年级', '小明', 'xiaoming@666.com', '13666666666', '这个年级的主任是小明');
+INSERT INTO `tb_grade` VALUES (1, '22级', '申', 'shen@qq.com', '13866666666', '大学二年级');
 
 -- ----------------------------
 -- Table structure for tb_student
@@ -113,14 +103,10 @@ CREATE TABLE `tb_student`  (
 -- ----------------------------
 -- Records of tb_student
 -- ----------------------------
-INSERT INTO `tb_student` VALUES (1, '1001', '张小明', '男', 'e10adc3949ba59abbe56e057f20f883e', 'yinyufei@163.com', '13866666666', '北京天通苑', '这个学生学习好', 'upload/default.jpg', '一年一班');
-INSERT INTO `tb_student` VALUES (2, '1002', '郭建超', '男', 'e10adc3949ba59abbe56e057f20f883e', 'guojianchao@163.com', '13866666666', '北京昌平', '这个学生会功夫', 'upload/default.jpg', '一年一班');
-INSERT INTO `tb_student` VALUES (3, '1003', '史汶鑫', '男', 'e10adc3949ba59abbe56e057f20f883e', 'shiwenxin@163.com', '13866666666', '北京昌平', '这个学生酒量好', 'upload/default.jpg', '二年一班');
-INSERT INTO `tb_student` VALUES (4, '1004', '高建军', '男', 'e10adc3949ba59abbe56e057f20f883e', 'gaojianjun@163.com', '13866666666', '北京昌平', '这个学生会做饭', 'upload/default.jpg', '二年一班');
-INSERT INTO `tb_student` VALUES (5, '1005', '邹伟斌', '男', 'e10adc3949ba59abbe56e057f20f883e', 'zouweibin@163.com', '13866666666', '北京昌平', '这个学生能吃辣', 'upload/default.jpg', '三年一班');
-INSERT INTO `tb_student` VALUES (6, '1006', '刘路', '男', 'e10adc3949ba59abbe56e057f20f883e', 'liulu@163.com', '13866666666', '北京昌平', '这个学生是学霸', 'upload/default.jpg', '三年二班');
-INSERT INTO `tb_student` VALUES (7, '1007', '庞家仨', '女', 'e10adc3949ba59abbe56e057f20f883e', 'pangjiasan@163.com', '13866666666', '北京昌平', '这个学生海拔高', 'upload/default.jpg', '三年二班');
-INSERT INTO `tb_student` VALUES (8, '1008', '谭帅333', '男', 'e10adc3949ba59abbe56e057f20f883e', 'tanshuai@163.com', '13866666666', '北京昌平', '这个学生想考研', 'upload/default.jpg', '四年一班');
+INSERT INTO `tb_student` VALUES (1, '1001', '叶伟轩', '男', 'e10adc3949ba59abbe56e057f20f883e', '1930721926@qq.com', '13866666666', '湖南湘潭', '1', 'upload/default.jpg', '22计算机科学与技术三班');
+INSERT INTO `tb_student` VALUES (2, '1002', '欧锦聪', '男', 'e10adc3949ba59abbe56e057f20f883e', '1256075513@qq.com', '13866666666', '湖南湘潭', '2', 'upload/default.jpg', '22计算机科学与技术三班');
+INSERT INTO `tb_student` VALUES (3, '1003', '申林斌', '男', 'e10adc3949ba59abbe56e057f20f883e', '3588847064@qq.com', '13866666666', '湖南湘潭', '3', 'upload/default.jpg', '22计算机科学与技术三班');
+INSERT INTO `tb_student` VALUES (4, '1004', '杨黄燊', '男', 'e10adc3949ba59abbe56e057f20f883e', '3575789958@qq.com', '13866666666', '湖南湘潭', '4', 'upload/default.jpg', '22计算机科学与技术三班');
 
 -- ----------------------------
 -- Table structure for tb_teacher
@@ -143,13 +129,10 @@ CREATE TABLE `tb_teacher`  (
 -- ----------------------------
 -- Records of tb_teacher
 -- ----------------------------
-INSERT INTO `tb_teacher` VALUES (1, '101', '大圣', '女', 'e10adc3949ba59abbe56e057f20f883e', 'dasheng@163.com', '13866666666', '北京昌平', 'upload/default.jpg', '一年一班');
-INSERT INTO `tb_teacher` VALUES (2, '102', '小张', '男', 'e10adc3949ba59abbe56e057f20f883e', 'xiaozhang@163.com', '13866666666', '北京海淀', 'upload/default.jpg', '一年二班');
-INSERT INTO `tb_teacher` VALUES (3, '103', '小韩', '男', 'e10adc3949ba59abbe56e057f20f883e', 'xiaohan@163.com', '13866666666', '北京朝阳', 'upload/default.jpg', '二年一班');
-INSERT INTO `tb_teacher` VALUES (4, '104', '小强', '男', 'e10adc3949ba59abbe56e057f20f883e', 'xiaoqiang@163.com', '13866666666', '北京通州', 'upload/default.jpg', '二年二班');
-INSERT INTO `tb_teacher` VALUES (5, '105', '小花', '男', 'e10adc3949ba59abbe56e057f20f883e', 'xiaohua@163.com', '13866666666', '北京顺义', 'upload/default.jpg', '三年一班');
-INSERT INTO `tb_teacher` VALUES (6, '106', '小赵', '男', 'e10adc3949ba59abbe56e057f20f883e', 'xiaozhao@163.com', '13866666666', '北京东城', 'upload/default.jpg', '三年二班');
-INSERT INTO `tb_teacher` VALUES (7, '107', '小飞', '男', 'e10adc3949ba59abbe56e057f20f883e', 'xiaofei@163.com', '13866666666', '北京西城', 'upload/default.jpg', '四年一班');
-INSERT INTO `tb_teacher` VALUES (8, '108', '秀秀2', '男', 'e10adc3949ba59abbe56e057f20f883e', '123456@123.com', '13855555555', '海淀', 'upload/2977694e8da847e2a3e6f0af9416fbba.png', '一年二班');
+INSERT INTO `tb_teacher` VALUES (1, '101', '欧', '男', 'e10adc3949ba59abbe56e057f20f883e', 'ou@163.com', '13866666666', '湖南湘潭', 'upload/default.jpg', '22计算机科学与技术一班');
+INSERT INTO `tb_teacher` VALUES (2, '102', '叶', '男', 'e10adc3949ba59abbe56e057f20f883e', 'ye@163.com', '13866666666', '湖南湘潭', 'upload/default.jpg', '22计算机科学与技术二班');
+INSERT INTO `tb_teacher` VALUES (3, '103', '申', '男', 'e10adc3949ba59abbe56e057f20f883e', 'shen@163.com', '13866666666', '湖南湘潭', 'upload/default.jpg', '22计算机科学与技术三班');
+INSERT INTO `tb_teacher` VALUES (4, '104', '杨', '男', 'e10adc3949ba59abbe56e057f20f883e', 'yang@163.com', '13866666666', '湖南湘潭', 'upload/default.jpg', '22计算机科学与技术四班');
+
 
 SET FOREIGN_KEY_CHECKS = 1;

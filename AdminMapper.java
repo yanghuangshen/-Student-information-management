fe4420ef5d51482f1a622b5c5c459f8cf5c7c392
yanghuangshen <144525518+yanghuangshen@users.noreply.github.com>
2024-06-03 15:22:50 +0800

    后端mapper

---
+++ AdminMapper.java
@@ -0,0 +1,10 @@
+package com.fxy.campus.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fxy.campus.pojo.Admin;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminMapper extends BaseMapper<Admin> {
+}
---
+++ TeacherMapper.java
@@ -0,0 +1,9 @@
+package com.fxy.campus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fxy.campus.pojo.Teacher;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherMapper extends BaseMapper<Teacher> {
+}
